e15a4c1d02f7dc60cca6538f03f925848a3eae17
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-06-16 10:00:52 +0530

    Changed default font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Recursive Mono Casual Static:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
